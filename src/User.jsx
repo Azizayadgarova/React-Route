da93e8d754be137b54bcaaa20c2a67273c98b2a1
yadgarovtthjjiiiiiiiiiiiiiiiiiiii <mamuray819@gmail.com>
2025-09-09 10:08:08 +0500

    done

--- src/User.jsx
+++ src/User.jsx
@@ -18,7 +18,7 @@ const User = () => {
     return (
         <div className='w-50 mx-auto mb-5'>
             <div className='border p-3 mt-5'>
-                <p className='text-center fs-3'>Register</p>
+                <p className='text-center fs-3 text-bl'>Register</p>
                 <div className='d-flex gap-3'>
                     <input onChange={e => setfistName(e.target.value)} type="text" className={`form-control ${ValidateColorFirstName}`} placeholder='First Name' value={firstName} />
                     <input onChange={e => setLastname(e.target.value)} type="text" className={`form-control ${ValidateColorLastName}`} placeholder='Last Name' value={lastName} />
